5b456fce856c4d575306e1ef4e58fdf8312d954b
Swathi Gangaraju <56689314+swathihg@users.noreply.github.com>
2025-02-24 12:45:50 +1000

    Add files via upload

---
+++ NDA_data_download/failed_download.sh
@@ -0,0 +1,25 @@
+failed download
+
+#!/bin/bash --login
+#SBATCH --nodes=1                # Request 1 node
+#SBATCH --ntasks-per-node=10     # Number of tasks per node
+#SBATCH --cpus-per-task=4        # Number of CPUs per task
+#SBATCH --mem=10G                # Memory per node
+#SBATCH --job-name=failed_download       # Job name
+#SBATCH --time=100:00:00           # Maximum runtime
+#SBATCH --partition=general       # Partition to submit to
+#SBATCH --account=        # Account to charge the job
+#SBATCH -o slurm-%j.output  # Standard output file
+#SBATCH -e slurm-%j.error   # Standard error file
+
+module load python
+
+pip install nda-tools
+
+pip install nda-tools --user
+
+pip install secretstorage --upgrade keyrings.alt
+
+echo -e "name\npassword" | downloadcmd -dp 1235636 -u name -d output_path -wt 191 -t "failed_csv_file_path"
+
+wait
